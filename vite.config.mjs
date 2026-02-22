6b7596d640086f6ca8a8bab260c771f9931cafc7
Yanran Wang <wyr20031104@gmail.com>
2026-02-23 00:27:00 +0800

    promo deploy 1st try

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/pure-comment/",
   plugins: [
     react(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        promo: resolve(__dirname, "promo.html"),
+      },
+    },
+  },
 })

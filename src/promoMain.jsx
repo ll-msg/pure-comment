0878c384aa6f36f641972038832d1d386b376b59
Yanran Wang <wyr20031104@gmail.com>
2026-02-23 08:59:22 +0800

    update promo deploy

--- src/promoMain.jsx
+++ src/promoMain.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Promo from "./pages/Promo.jsx";
-import Contact from "./pages/Contact.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
